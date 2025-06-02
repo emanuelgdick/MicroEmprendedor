e81bc552dd64c3aea9592ca291f6563f974231b7
Emanuel <emanuelgdick@gmail.com>
2025-06-02 12:42:24 -0300

    sss

--- FrontEnd/Controllers/SocioController.cs
+++ FrontEnd/Controllers/SocioController.cs
@@ -41,6 +41,14 @@ namespace FrontEnd.Controllers
 
 
         [Authorize(Roles = "Admin")]
+        public async Task<JsonResult> GetSociosFiltrados(int calle,int tipo,int categoria,int estado)
+        {
+            List<Socio> oLista = new List<Socio>();
+            oLista = await _apiService.GetSociosFiltrados(calle,tipo,categoria,estado,HttpContext.Session.GetString("APIToken"));
+            return Json(new { data = oLista });
+        }
+
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Create()
         {
             return View();
