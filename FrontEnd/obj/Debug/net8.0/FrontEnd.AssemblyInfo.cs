06946e7ce73d79ad492939616b55da99bd064593
Emanuel <emanuelgdick@gmail.com>
2025-06-03 07:13:36 -0300

    Ignore

--- FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538568bb65adfbf0ddec34cba09b3e840dcf0d55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0567db6b7b79baf344bdb8c72ebf078fdc7cbc1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
