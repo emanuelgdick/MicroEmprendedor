8416f659145e7304f4aecb72d20b3dbc7564f70c
Emanuel <emanuelgdick@gmail.com>
2025-07-25 12:48:15 -0300

    qqqq

---
+++ Api/Models/MicroEmprendedorRubro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models
+{
+    public class MicroEmprendedorRubro
+    {
+       
+            [Key]
+            public int Id { get; set; }
+
+           [ForeignKey("IdMicroEmprendedor")]
+            public int IdMicroEmprendedor { get; set; }
+          //  public MicroEmprendedor? MicroEmprendedor { get; set; }
+
+            [ForeignKey("IdRubro")]
+            public int IdRubro { get; set; }
+          //  public Rubro? Rubro { get; set; }
+       
+    }
+}
---
+++ FrontEnd/Models/MicroEmprendedorRubro.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using FrontEnd.Models;
+
+namespace FrontEnd.Models
+{
+    public class MicroEmprendedorRubro
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("IdMicroEmprendedor")]
+        public int IdMicroEmprendedor { get; set; }
+        //public MicroEmprendedor? MicroEmprendedor { get; set; }
+
+        [ForeignKey("IdRubro")]
+        public int IdRubro { get; set; }
+        //public Rubro? Rubro { get; set; }
+
+    }
+}
