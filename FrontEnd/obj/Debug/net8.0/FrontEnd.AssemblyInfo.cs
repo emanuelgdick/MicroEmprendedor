17ceb4b8f0538a5dec7cabc4b88bdf88e4d2a47b
Emanuel <emanuelgdick@gmail.com>
2025-09-10 15:10:27 -0300

    Commit 10092025 1510

--- FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea4b58901051b3af759024ee9160db061b5e2a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47abc95816d09133e79a07a69523c6e339dc8a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
